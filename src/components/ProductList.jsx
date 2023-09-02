a24496d9a563c7eefbde60800eb5bee0d09e7bcc
syedtahasohail <syed.taha.sohail@hotmail.com>
2023-09-02 12:49:48 +0500

    Adding Loader in Fetching the records.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,28 +1,44 @@
 import { useEffect, useState } from "react";
-
+import  { Triangle }  from 'react-loader-spinner';
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+//API Calling 
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then(response => response.json())
-      .then(data => setProducts(data));
-
+    setLoading(true);
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+        setProducts(data);
+      })
+      .catch(function (ex) {
+        setLoading(false);
+        console.log(ex.message);
+      });
   }, []);
 
-  return (
-    <div className>
-      <h1>Products</h1>
-      <div className="post-container">
-        {products.map(product => (
-          <div key={product.title}>
-            <h2 className="post-title">Title : {product.title}</h2>
-            <p className="post-body">Price : {product.price}</p>
-            <p className="post-body">Description : {product.description}</p>
 
-          </div>
-        ))}
-      </div>
+  // Rendring
+  return (
+    <div>
+      {loading ? (
+        <Triangle type="Puff" color="#00BFFF" height={'auto'} width={'auto'} />
+      ) : (
+        <div>
+          <h1>Products</h1>
+          {products.map((product) => (
+            <div key={product.id}>
+              <img src={product.image} height={'150px'} width={'150px'} />
+              <h3 className="post-title">Title : {product.title}</h3>
+              <p className="post-body">Price : {product.price}</p>
+              <p className="post-body">Description : {product.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
